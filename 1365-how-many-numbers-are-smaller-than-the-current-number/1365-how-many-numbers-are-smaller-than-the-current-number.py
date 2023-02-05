d1dcf5c02800728caf8370c08188ef75cbb9363a
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2023-02-05 14:53:28 +0530

    Time: 61 ms (81.94%), Space: 13.9 MB (39.89%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -13,13 +13,31 @@ class Solution:
         # return ans
                     
         # Method 2 
-        dic={}
-        for k,v in enumerate(sorted(nums)):
-            if v not in dic:
-                dic[v]=k
+        # Time Complexity of O(nlogn)
+        # dic={}
+        # for k,v in enumerate(sorted(nums)):
+        #     if v not in dic:
+        #         dic[v]=k
+        # ans=[]
+        # for item in nums:
+        #     ans.append(dic[item])
+        # return ans
+            
+        # Method 3
+        # Time Complexity of O(n)
+        m=max(nums)
+        dummy=[0]*(max(nums)+1)
+        n=len(nums)
+        for i in range(n):
+            dummy[nums[i]] +=1
+        print(dummy)
+        for i in range(1,len(dummy)):
+            dummy[i] +=dummy[i-1]
+        print(dummy)
         ans=[]
-        for item in nums:
-            ans.append(dic[item])
+        for i in range(n):
+            if nums[i] !=0:
+                ans.append(dummy[nums[i]-1])
+            else:
+                ans.append(0)
         return ans
-            
-            
