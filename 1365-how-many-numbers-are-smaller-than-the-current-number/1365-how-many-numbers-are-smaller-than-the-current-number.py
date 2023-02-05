5b8e3a834047d40eda432a8ed3328d91cc96b145
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2023-02-05 14:22:54 +0530

    Time: 58 ms (85.97%), Space: 13.8 MB (80.98%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,13 +1,25 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        ans=[]
-        n=len(nums)
-        for i in range(n):
-            c=0
-            for j in range(n):
-                if j !=i and nums[j]<nums[i]:
-                    c +=1
-            ans.append(c)
-        return ans
-                    
         
+        # Method 1 Brute force approach O(n*n) Time coplexity
+        # ans=[]
+        # n=len(nums)
+        # for i in range(n):
+        #     c=0
+        #     for j in range(n):
+        #         if j !=i and nums[j]<nums[i]:
+        #             c +=1
+        #     ans.append(c)
+        # return ans
+                    
+        # Method 2 
+        dic={}
+        for k,v in enumerate(sorted(nums)):
+            if v not in dic:
+                dic[v]=k
+        ans=[]
+        for item in nums:
+            ans.append(dic[item])
+        return ans
+            
+            
