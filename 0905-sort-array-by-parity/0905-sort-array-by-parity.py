bb6c0a4c09c583d377b6425ed63c4e18e25fd043
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2023-02-05 15:35:47 +0530

    Time: 84 ms (65.05%), Space: 14.7 MB (54.02%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -13,4 +13,17 @@ class Solution:
         # return ans
         
         # Method 1 with different Code
-        return [i for i in nums if i%2==0] + [i for i in nums if i%2 !=0]
+        # return [i for i in nums if i%2==0] + [i for i in nums if i%2 !=0]
+        
+        # Method 2
+        i=0
+        j=len(nums)-1
+        while i <j:
+            while nums[i]%2 !=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j -=1
+                if j<=i:
+                    break
+            i +=1
+        return nums
+                
