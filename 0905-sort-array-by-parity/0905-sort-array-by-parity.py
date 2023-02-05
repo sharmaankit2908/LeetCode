27cb67f38d5f4cbb4b9aabed776d09cf0a3e082e
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2023-02-05 15:19:29 +0530

    Time: 74 ms (94.50%), Space: 14.7 MB (12.62%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,11 +1,16 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        ans=[]
-        n=len(nums)
-        for i in range(len(nums)):
-            if nums[i]%2==0:
-                ans.append(nums[i])
-        for i in range(len(nums)):
-            if nums[i]%2!=0:
-                ans.append(nums[i])
-        return ans
+        # Method 1 
+        # Time complexity of O(n)
+        # ans=[]
+        # n=len(nums)
+        # for i in range(len(nums)):
+        #     if nums[i]%2==0:
+        #         ans.append(nums[i])
+        # for i in range(len(nums)):
+        #     if nums[i]%2!=0:
+        #         ans.append(nums[i])
+        # return ans
+        
+        # Method 1 with different Code
+        return [i for i in nums if i%2==0] + [i for i in nums if i%2 !=0]
