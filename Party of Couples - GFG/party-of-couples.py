af32c2cff59a6cbd19b2082ca546753e06494635
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2023-02-05 13:17:26 +0530

    Added solution - LeetHub

---
+++ Party of Couples - GFG/party-of-couples.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def findSingle(self, N, arr):
+        # code here
+        a=0
+        for i in range(N):
+            a ^=arr[i]
+        return a
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        arr = input().split()
+        for itr in range(N):
+            arr[itr] = int(arr[itr])
+        
+        ob = Solution()
+        print(ob.findSingle(N, arr))
+
+# } Driver Code Ends
